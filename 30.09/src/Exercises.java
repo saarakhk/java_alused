5aae2a8bfebd5aa6d5738c7331540c12e693469e
saarakhk <saara.vallik@khk.ee>
2019-09-30 14:37:39 +0300

    Exercise 1, 2, 3, 4: Name, Hello world! (And all the people of the world), Varying variables, Spruce

---
+++ 30.09/src/Exercises.java
@@ -0,0 +1,21 @@
+public class Exercises {
+    public static void main(String[] args) {
+        // Ex1
+        System.out.println("Saara Vällik");
+        // Ex2
+        System.out.println("Hello world!\n(And all the people of the world)");
+        // Ex3
+        System.out.println("    *    \n   ***   \n  *****  \n ******* \n*********\n    *    ");
+        // Ex4
+        int numberOne = 3;
+        double numberTwo = 5.5;
+        String stringOne = "There is none!";
+
+        System.out.println("Chickens:\n" + numberOne);
+        System.out.println("Bacon (kg):\n" + numberTwo);
+        System.out.println("A tractor:\n" + stringOne);
+        System.out.println(" ");
+        System.out.println("In a nutshell\n" + numberOne + "\n" + numberTwo + "\n" + stringOne);
+
+    }
+}
