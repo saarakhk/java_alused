2510f656ab06aaa94271685accd8aa173555b27e
saarakhk <saara.vallik@khk.ee>
2019-09-30 15:37:16 +0300

    Exercise 6: Addition; Exercise 7: Multiplication

--- 30.09/src/Exercises.java
+++ 30.09/src/Exercises.java
@@ -1,31 +1,17 @@
 public class Exercises {
     public static void main(String[] args) {
-        // Ex1
-        System.out.println("Saara Vällik");
-        // Ex2
-        System.out.println("Hello world!\n(And all the people of the world)");
-        // Ex3
-        System.out.println("    *    \n   ***   \n  *****  \n ******* \n*********\n    *    ");
-        // Ex4
-        int numberOne = 9000;
-        double numberTwo = 0.1;
-        String stringOne = "There is none!";
-        String stringTwo = "Zetor";
+    // Ex6
+        int numberOne = 1;
+        int numberTwo = 2;
+        int sum = numberOne + numberTwo;
 
-        System.out.println("Chickens:\n" + numberOne);
-        System.out.println("Bacon (kg):\n" + numberTwo);
-        System.out.println("A tractor:\n" + stringTwo);
-        System.out.println(" ");
-        System.out.println("In a nutshell\n" + numberOne + "\n" + numberTwo + "\n" + stringTwo);
+        System.out.println(numberOne + " + " + numberTwo + " = " + sum);
 
-        // Ex5
-        int daysInYear = 365;
-        int minutesInDay = 60 * 24;
-        int secondsInDay = minutesInDay * 60;
-        int secondsInYear = daysInYear * secondsInDay;
+     // Ex7
+        int numberThree = 3;
+        int numberFour = 4;
+        int sum2 = numberThree + numberFour;
 
-        System.out.println("There are " + secondsInYear + " seconds in a year.");
-
-
+        System.out.println(numberThree + " * " + numberFour + " = " + sum2);
     }
 }
