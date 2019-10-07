f7b188d1f1f84c4d5c48aca484b25f89f8e751fd
saarakhk <saara.vallik@khk.ee>
2019-10-07 14:13:39 +0300

    Exercise 18: Grades and points

--- 07.10/src/Exercises.java
+++ 07.10/src/Exercises.java
@@ -3,21 +3,25 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // Exercise 17: Greater number
+        // Exercise 18: Grades and points
 
-        System.out.print("Type the first number: ");
+        System.out.print("Type the points [0-60]: ");
         int num1 = Integer.parseInt(reader.nextLine());
-        System.out.print("Type the second number: ");
-        int num2 = Integer.parseInt(reader.nextLine());
 
-        int greaterNumber =  Math.max(num1,num2);
-
-        if (num1 > num2) {
-            System.out.println("Number " + num1 + " is bigger than " + num2);
-        } else if (num1 < num2) {
-            System.out.println("Number " + num2 + " is bigger than " + num1);
+        if (num1 <= 29) {
+            System.out.println("You failed the test!");
+        } else if (num1 <= 34 && num1 > 28) {
+            System.out.println("Your grade is 1");
+        } else if (num1 <= 39 && num1 > 34) {
+            System.out.println("Your grade is 2");
+        } else if (num1 <= 44 && num1 > 39 ) {
+            System.out.println("Your grade is 3");
+        } else if (num1 <= 49 && num1 > 44 ) {
+            System.out.println("Your grade is 4");
+        } else if (num1 <= 60 && num1 > 49) {
+            System.out.println("Your grade is 5");
         } else {
-            System.out.println("The numbers are equal!");
+            System.out.println("Something went wrong");
         }
     }
 } 
