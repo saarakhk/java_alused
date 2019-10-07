fa4f122dfd50600e84395d0295da0d817e2198ba
saarakhk <saara.vallik@khk.ee>
2019-10-07 13:57:05 +0300

    Exercise 17: Greater number

--- 07.10/src/Exercises.java
+++ 07.10/src/Exercises.java
@@ -3,15 +3,21 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // Exercise 16: Even or odd?
+        // Exercise 17: Greater number
 
-        System.out.print("Type a number: ");
+        System.out.print("Type the first number: ");
         int num1 = Integer.parseInt(reader.nextLine());
+        System.out.print("Type the second number: ");
+        int num2 = Integer.parseInt(reader.nextLine());
 
-        if (num1 % 2 == 0) {
-            System.out.println("Number " + num1 + " is even.");
+        int greaterNumber =  Math.max(num1,num2);
+
+        if (num1 > num2) {
+            System.out.println("Number " + num1 + " is bigger than " + num2);
+        } else if (num1 < num2) {
+            System.out.println("Number " + num2 + " is bigger than " + num1);
         } else {
-            System.out.println("Number " + num1 + " is odd.");
+            System.out.println("The numbers are equal!");
         }
     }
 } 
