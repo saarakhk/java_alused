c8e0a6de6f05d4a454df2d90e22948bd9e40d8ed
saarakhk <saara.vallik@khk.ee>
2019-10-07 13:35:00 +0300

    Exercise 15: Age of majority

--- 07.10/src/Exercises.java
+++ 07.10/src/Exercises.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // Exercise 14: A positive number
+        // Exercise 15: Age of majority
 
-        System.out.print("Type a number: ");
+        System.out.print("How old are you? ");
         int num1 = Integer.parseInt(reader.nextLine());
 
-        if (num1 > 0) {
-            System.out.println("The number is positive.");
+        if (num1 < 18) {
+            System.out.println("You have not reached the age of majority yet!");
         } else {
-            System.out.println("The number is not positive.");
+            System.out.println("You have reached the age of majority");
         }
     }
 } 
