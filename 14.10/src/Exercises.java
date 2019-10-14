f418e53aa60e22f033dc044877213f8df77bbf0c
saarakhk <saara.vallik@khk.ee>
2019-10-14 13:10:08 +0300

    Exercise 21 Leap year

--- 14.10/src/Exercises.java
+++ 14.10/src/Exercises.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // Exercise 20: Usernames
+        // Exercise 21 Leap year
 
-        System.out.print("What is your username? ");
-        String username = reader.nextLine();
-        System.out.print("What is your password? ");
-        String password = reader.nextLine();
+        System.out.println("Type a year: ");
+        int year = Integer.parseInt(reader.nextLine());
 
-        if (((username.equals("saara")) && password.equals("java")) || ((username.equals("admin")) && password.equals("pin"))) {
-            System.out.println("Oled sisselogitud");
+        if (year % 4 != 0) {
+            System.out.println("is not leap");
+        } else if (year % 400 == 0) {
+            System.out.println("is leap");
+        } else if (year % 100 == 0) {
+            System.out.println("is not leap");
         } else {
-            System.out.println("Sellist kasutajat ei ole või on sul vale salasõna");
+            System.out.println("is leap");
         }
 
     }
-} 
+}
