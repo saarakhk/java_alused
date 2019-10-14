5031c92e2137fed322a478c74d6130a6e31698e8
saarakhk <saara.vallik@khk.ee>
2019-10-14 14:52:06 +0300

    Exercise 27: From one to a hundred

---
+++ More loops/src/Exercises.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercises {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Exercise 27: From one to a hundred
+        int number = 1;
+
+        while (number < 101) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
