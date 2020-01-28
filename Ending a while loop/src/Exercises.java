3763bc70506562d5d9f317a123e6b62473e2a8fc
saarakhk <saara.vallik@khk.ee>
2020-01-28 22:39:07 +0200

    Exercise 38: Many prints

--- Ending a while loop/src/Exercises.java
+++ Ending a while loop/src/Exercises.java
@@ -2,37 +2,20 @@
 import java.util.Scanner;
 
 public class Exercises {
-    public static void main(String[] args) {
+    public static void main (String [] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Exercise 36.4 & 36.5: Counting the average & Even and odd numbers
-
-        System.out.println("Type numbers between 5 to -1: ");
-        int number;
-        int sum = 0;
-        int count = 0;
-        double average = 0;
-        int odd = 0;
-        int even = 0;
-
-        while (true) {
-            number = Integer.parseInt(reader.nextLine());
-            if(number == -1) {
-                System.out.println("Thank you and see you later!");
-                break;
-            } if (number % 2 == 0) {
-                even++;
-            } else {
-                odd++;
-            }
-            sum+= number;
-            count++;
-            average = (double) sum / count;
+        System.out.println("Hey, type a number:");
+        int result = Integer.parseInt(reader.nextLine());
+        int i= 0;
+        while (i<= result){
+            printText();
+            i++;
         }
-        System.out.println("Sum: " + sum);
-        System.out.println("Number of guesses: "+ count);
-        System.out.println("Avarage is: " + average); // without double
-        System.out.println("Even nr: " + even);
-        System.out.println("Odd nr: " + odd);
+
     }
+    public static void printText(){
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+
 }
