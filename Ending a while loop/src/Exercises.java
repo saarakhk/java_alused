78fb5b97ab834cc7322d6e25a7c8e54f731e31b6
saarakhk <saara.vallik@khk.ee>
2020-01-28 22:51:15 +0200

    Exercise 58: Recurring word & Exercise 60: Words in alphabetical order

--- Ending a while loop/src/Exercises.java
+++ Ending a while loop/src/Exercises.java
@@ -1,26 +1,30 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Exercises {
     public static void main(String [] args) {
+        ArrayList<String> words = new ArrayList<String>();
         Scanner reader = new Scanner(System.in);
-        System.out.println("Write a  first word: ");
-        String first = reader.nextLine();
-        System.out.println("How about a another word:");
-        String second = reader.nextLine();
+        String repeatedWord;
 
-        int secLeng = second.length();
+        while (true) {
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
 
-        int index = first.indexOf(second);
-        System.out.println(index);
+            if (words.contains(word)) {
+                repeatedWord = word;
+                break;
+            }else {
+                words.add(word);
+            }
 
-        if (index == -1 ) {
-            System.out.println("The word '" + second + "' is not found in the word '" + first + "'.");
-        }else {
-            System.out.println("The word '" + second + "' is found in the word '" + first + "'.");
+        } // while ends
+        System.out.println("You cave the word " + repeatedWord + " twice.");
+        Collections.sort(words);
+        for (String word : words) {
+            System.out.println(word);
         }
-
-
-
     }
 }
