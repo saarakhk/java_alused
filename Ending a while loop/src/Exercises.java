0d6275e4a40939fd7711275667d095f39da8a418
saarakhk <saara.vallik@khk.ee>
2020-01-28 22:43:56 +0200

    Exercise 44: Least

--- Ending a while loop/src/Exercises.java
+++ Ending a while loop/src/Exercises.java
@@ -2,54 +2,17 @@
 import java.util.Scanner;
 
 public class Exercises {
-    // printing stars and whitespaces
-    public static void main (String [] args){
-        xmasTree(8);
-
-
+    public static void main(String[] args) {
+        int answer =  least(9, 7);
+        System.out.println("Least: " + answer);
     }
-
-    public static void printWhitespace (int size) {
-        int i= 0;
-//        while (size > 0){
-//            System.out.print(" ");
-//            size--;
-//        }
-        while (i < size){
-            System.out.print(" ");
-            i++;
+    public static int least(int number1, int number2) {
+        // return smallest number
+        if (number1 >  number2) {
+            return number2;
+        }else {
+            return number1;
         }
-    }
-    public static void printStars (int size) {
-        int i= 0;
-        while (i < size){
-            System.out.print("*");
-            i++;
-        }
-
-    }
-    public static void xmasTree (int size) {
-        int i= 1;
-        int end = (size * 2) - 1;
-        int wsSum = (end - 1) / 2;
-
-        while (i<= end){
-            printWhitespace(wsSum);
-            printStars(i);
-            System.out.println();
-            wsSum-=1;
-            i+=2;
-
-        }
-        int a = 0;
-        int trunkWS = (end-3)/2;
-        while (a < 2) {
-            printWhitespace(trunkWS);
-            printStars(3);
-            System.out.println();
-            a++;
-        }
-
     }
 
 }
