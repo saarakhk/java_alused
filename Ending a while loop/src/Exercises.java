8d6b3917b881b0b2a3afc6f2e87442bd41d3eb35
saarakhk <saara.vallik@khk.ee>
2020-01-28 22:55:28 +0200

    Exercise 66: The Greatest

--- Ending a while loop/src/Exercises.java
+++ Ending a while loop/src/Exercises.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Exercises {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(3);
@@ -12,16 +12,17 @@ public class Exercises {
         list.add(7);
         list.add(2);
 
-        System.out.println("The average is: " + average(list));
-    }
-    public static double average (ArrayList<Integer> list){
-        double sum = 0;
-        for (double nr:list){
-            sum+=nr;
 
+
+        System.out.println("The greatest number is: " + greatest(list));
+        for (int nr : list){
+            System.out.println(nr);
         }
-        double endSum = sum / list.size();
 
-        return endSum;
+    }
+
+    public static int greatest(ArrayList<Integer> list) {
+        Collections.sort(list);
+        return list.get(list.size()-1);
     }
 }
