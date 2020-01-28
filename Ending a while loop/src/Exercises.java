e8a3a983ac649393378adb36b240014283c8e0ec
saarakhk <saara.vallik@khk.ee>
2020-01-28 22:41:17 +0200

    Exercise 39: Printing

--- Ending a while loop/src/Exercises.java
+++ Ending a while loop/src/Exercises.java
@@ -2,20 +2,43 @@
 import java.util.Scanner;
 
 public class Exercises {
-    public static void main (String [] args) {
-        Scanner reader = new Scanner(System.in);
+    public static void main (String [] args){
+        printHalfTriangle(5);
 
-        System.out.println("Hey, type a number:");
-        int result = Integer.parseInt(reader.nextLine());
+    }
+    public static void printSquare (int size) {
         int i= 0;
-        while (i<= result){
-            printText();
+        while (i < size) {
+            printStars(size);
+            System.out.println();
+            i++;
+        }
+    }
+    public static void printRectangle (int width, int height) {
+        int i= 0;
+        while (i < height) {
+            printStars(width);
+            System.out.println();
+            i++;
+        }
+    }
+    public static void printHalfTriangle (int size) {
+        int i= 0;
+        while (i < size) {
+            printStars(i);
+            System.out.println();
+            i++;
+        }
+    }
+
+
+    public static void printStars (int amount) {
+        int i= 0;
+        while (i <= amount){
+            System.out.print("*");
             i++;
         }
 
-    }
-    public static void printText(){
-        System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 
 }
