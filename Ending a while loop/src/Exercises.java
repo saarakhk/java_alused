20f931bf33f16be4f9fbd320a9e0c50880e3ee6e
saarakhk <saara.vallik@khk.ee>
2020-01-28 22:45:54 +0200

    50 and 55 Type name first three letters and type it reversed

--- Ending a while loop/src/Exercises.java
+++ Ending a while loop/src/Exercises.java
@@ -2,17 +2,30 @@
 import java.util.Scanner;
 
 public class Exercises {
-    public static void main(String[] args) {
-        int answer =  least(9, 7);
-        System.out.println("Least: " + answer);
-    }
-    public static int least(int number1, int number2) {
-        // return smallest number
-        if (number1 >  number2) {
-            return number2;
-        }else {
-            return number1;
+    public static void main(String [] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Hei, type here your name: ");
+        String name = reader.nextLine();
+        System.out.println("name lenght: "+ name.length());
+        // reversing the name
+        int i = name.length()-1;
+
+
+
+        if (name.length() <= 2) {
+            System.out.println("Uuf such a short name.");
+
+        } else {
+            System.out.println("1. character: "+ name.charAt(0));
+            System.out.println("2. character: "+ name.charAt(1));
+            System.out.println("3. character: "+ name.charAt(2));
+            System.out.println("In reverse order: ");
+            while (i >= 0){
+                System.out.print(name.charAt(i));
+                i--;
+            }
         }
+
     }
 
 }
